a0201b9fd1ff49869a4dfdb8d460bb2777ee6a58
brucewang15 <bruce.wang15@outlook.com>
2026-06-09 00:42:08 -0400

    intervals question

---
+++ leetcode/intervals/57_insert_intervals.py
@@ -0,0 +1,28 @@
+class Solution:
+    def insert(self, intervals: list[list[int]], newInterval: list[int]) -> list[list[int]]:
+        merged = []
+
+        for i in range(len(intervals)):
+            current = intervals[i]
+
+            if current[1] < newInterval[0]:
+                merged.append(current)
+            elif current[1] >= newInterval[0] and current[0] <= newInterval[1]:
+                merged.append([min(newInterval[0], current[0]), max(newInterval[1], current[1])])
+                for k in range(i+1, len(intervals), 1):
+                    current = intervals[k]
+                    if merged[-1][1] < current[0]:
+                        merged.extend(intervals[k:])
+                        break
+                    merged[-1][1] = max(merged[-1][1], current[1])
+                return merged
+            elif current[0] > newInterval[1]:
+                merged.append(newInterval)
+                merged.extend(intervals[i:])
+                return merged
+            
+        
+        merged.append(newInterval)
+        return merged
+
+        
