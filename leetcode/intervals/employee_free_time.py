9e46fe49aa5ec3a914a2cf42859830270ba16829
brucewang15 <bruce.wang15@outlook.com>
2026-06-21 13:40:59 -0400

    new heap approach for employee free time

--- leetcode/intervals/employee_free_time.py
+++ leetcode/intervals/employee_free_time.py
@@ -1,24 +1,37 @@
+import heapq
+
 class Solution:
     def employeeFreeTime(self, schedule: list[list[list[int]]]) -> list[list[int]]:
-        pointers = [[i, 0, len(schedule[i]), schedule[i][0][0]] for i in range(len(schedule))]
+        heap = []
         flattened = []
-        while pointers != []:
-            i, pointer, max_pointer, start = min(pointers, key=lambda x: x[3])
-            flattened.append(schedule[i][pointer])
-            if pointer + 1 < max_pointer:
-                pointers[i][1] += 1
-                pointers[i][3] = schedule[i][pointer + 1][0]
-            else:
-                pointers.pop(i)
-        print(flattened)
-
         free = []
 
-        prev = flattened[0]
+        # methods:
+        # heapq.heapify(list): o(n)
+        # heapq.heappush(heap, item): log(n)
+        # heapq.heappop(heap): log(n) returns smallest value and restore heap property
+
+        # generally, heaps are used for integers. In python, heaps can be used for strings and tuples as well. For tuples, heapq will compare the first index first, then second, etc. So store priority number in the first index. 
+
+        # o(k^2)
+        # in format: (start time, employee index, employee intv index)
+        for i in range(len(schedule)):
+            heapq.heappush(heap, (schedule[i][0][0], i, 0))
+        
+        # o(nklog(k))
+        while heap != []:
+            _, i, ei = heapq.heappop(heap)
+            flattened.append(schedule[i][ei])
+            if ei < len(schedule[i]) - 1:
+                heapq.heappush(heap, (schedule[i][ei+1][0], i, ei+1))
+
+        # prev = flattened[0] means prev references the original schedule. Hence modiying prev will have side effects.
+        # a better alternative is to create a new list object from the first flattened index. list() will iterate through the iterable provided and copy the array into a new object. This way no side effects.
+        prev = list(flattened[0])
         for i in range(1, len(flattened), 1):
             if prev[1] < flattened[i][0]:
                 free.append([prev[1], flattened[i][0]])
-                prev = flattened[i]
+                prev = list(flattened[i]) # o(2). constant
             else:
                 prev[1] = max(prev[1], flattened[i][1])
         return free
