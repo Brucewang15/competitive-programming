7a6bd653f4268efe944ec460d7a5a29366901b4f
brucewang15 <bruce.wang15@outlook.com>
2026-06-23 17:59:46 -0400

    decode strings python recursion

---
+++ leetcode/stack-queues/394_Decode_Strings.py
@@ -0,0 +1,50 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        decoded, _ = self.decode(s, 0)
+        return decoded
+    def decode(self, s: str, i: int) -> str:
+        decoded = ""
+        curNumber = 0
+        while (i < len(s)):
+            if s[i] >= 'a' and s[i] <= 'z':
+                decoded += s[i]
+            elif s[i].isdigit():
+                curNumber = curNumber * 10 + int(s[i])
+            elif s[i] == '[':
+                tempDecoded, k = self.decode(s, i+1)
+                decoded += curNumber * tempDecoded
+                i = k
+                curNumber = 0
+            else: # ]
+                break
+            i += 1
+        return decoded, i
+        # decoded = ""
+        # stack = []
+
+        # i = 0
+
+        # while i < len(s):
+        #     tempdigit=""
+        #     tempchars=""
+
+        #     while s[i].isdigit():
+        #         tempdigit += s[i]
+        #         i+=1
+        #     if tempdigit != "" and s[i] == '[':
+        #         stack.append(int(tempdigit))
+        #         i+=1
+        #     while i < len(s) and s[i] >= 'a' and s[i] <= 'z':
+        #         tempchars += s[i]
+        #         i+=1
+        #     if tempchars != "":
+        #         stack.append(tempchars)
+        #     if i < len(s) and s[i] == ']':
+        #         encoded = stack.pop()
+        #         k = stack.pop()
+        #         if len(stack) == 0:
+        #             decoded += k*encoded
+        #         else:
+        #             stack[-1] += k*encoded
+        #         i+=1
+        # return decoded
