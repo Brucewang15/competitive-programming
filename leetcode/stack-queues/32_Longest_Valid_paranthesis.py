590cd4d857a0c2ffa3d9d8c9cc796dbc1230e3f6
brucewang15 <bruce.wang15@outlook.com>
2026-06-27 21:29:51 -0400

    3 new sols for longest valid parentheses

--- leetcode/stack-queues/32_Longest_Valid_paranthesis.py
+++ leetcode/stack-queues/32_Longest_Valid_paranthesis.py
@@ -1,5 +1,70 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
+
+        # Solution 4
+        # Stack stores indexes of boundaries for a substring that ends at index i
+        longest = 0
+        stack = [-1]
+
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    longest = max(longest, i - stack[-1])
+        return longest
+
+        # Solution 3
+        # Bidirectional scan to fix asymmetry. Easier to detect excess ) than (
+        stack = 0
+        longest = 0
+        curStart = 0
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack += 1
+            elif s[i] == ')' and stack == 0:
+                curStart = i + 1
+            elif s[i] == ')':
+                stack -= 1
+            if stack == 0:
+                longest = max(longest, i - curStart + 1)
+
+        stack = 0
+        curStart = len(s) - 1
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == ')':
+                stack += 1
+            elif s[i] == '(' and stack == 0:
+                curStart = i - 1
+            elif s[i] == '(':
+                stack -= 1
+            if stack == 0:
+                longest = max(longest, curStart - i + 1)
+        return longest
+
+        # Solution 2
+        # Check for running maximum of longest substring, stopping at first invalid )
+        # Then check indexes of left over (
+        longest = 0
+        stack = []
+        curStart = 0
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)
+            elif s[i] == ')' and not stack:
+                curStart = i + 1
+            elif s[i] == ")" and stack:
+                stack.pop()
+            if not stack:
+                longest = max(longest, i - curStart + 1)
+        stack.append(len(s))
+        for i in range(1, len(stack), 1):
+            longest = max(longest, stack[i] - stack[i-1] - 1)
+        return longest
+
         longest = 0
         i = 0
         while i < len(s):
