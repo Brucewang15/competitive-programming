5ffec115a994b9f203d43a8828045542f0ed7f4d
brucewang15 <bruce.wang15@outlook.com>
2026-06-29 15:17:03 -0400

    updated sol

--- leetcode/stack-queues/853_Car_Fleet.py
+++ leetcode/stack-queues/853_Car_Fleet.py
@@ -1,18 +1,27 @@
 class Solution:
     def carFleet(self, target: int, position: list[int], speed: list[int]) -> int:
-        merged = [(position[i], speed[i]) for i in range(len(position))]
+        merged = list(zip(position, speed))
         merged.sort(key=lambda x: x[0])
         stack = []
         for (pos, s) in merged:
-            t = 0
             while stack and stack[-1][1] > s:
-                ppos, ps, pt = stack[-1]
-                time_to_merge = (pos - (ppos + ps * (t - pt))) / (ps - s)
-                if pos + time_to_merge * s <= target:
-                    pos, s, t = pos + time_to_merge * s, s, time_to_merge + t
+                time = (target - pos) / s
+                if stack[-1][0] + stack[-1][1] * time >= target:
                     stack.pop()
                 else:
                     break
-            stack.append((pos, s, t))
-
+            stack.append((pos, s))
         return len(stack)
+        # for (pos, s) in merged:
+        #     t = 0
+        #     while stack and stack[-1][1] > s:
+        #         ppos, ps, pt = stack[-1]
+        #         time_to_merge = (pos - (ppos + ps * (t - pt))) / (ps - s)
+        #         if pos + time_to_merge * s <= target:
+        #             pos, s, t = pos + time_to_merge * s, s, time_to_merge + t
+        #             stack.pop()
+        #         else:
+        #             break
+        #     stack.append((pos, s, t))
+
+        # return len(stack)
